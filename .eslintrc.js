4b5f74de6ef87e14ca04be1c43256fdcade44794
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-02-01 12:41:45 +0100

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,32 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb', 'prettier', 'plugin:prettier/recommended'],
-  plugins: ['import', 'prettier'],
+  extends: ['airbnb', 'prettier'],
+  plugins: ['import', 'promise', 'prettier'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 12,
+    sourceType: 'module',
   },
   rules: {
-    'class-methods-use-this': false,
+    // Native
+    'class-methods-use-this': [0],
+    'new-cap': 0,
+    'no-restricted-syntax': 0,
+    'no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+
+    // Plugins
+    'promise/always-return': 'error',
+    'promise/avoid-new': 'warn',
+    'promise/catch-or-return': 'error',
+    'promise/no-callback-in-promise': 'warn',
+    'promise/no-native': 'off',
+    'promise/no-nesting': 'warn',
+    'promise/no-new-statics': 'error',
+    'promise/no-promise-in-callback': 'warn',
+    'promise/no-return-in-finally': 'warn',
+    'promise/no-return-wrap': 'error',
+    'promise/param-names': 'error',
+    'promise/valid-params': 'warn',
   },
 };
